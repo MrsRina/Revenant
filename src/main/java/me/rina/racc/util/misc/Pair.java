266373af01875344df5359bdd0fb50f8ded3ce13
i0x75 <55637741+uwuhentai@users.noreply.github.com>
2020-10-07 08:52:49 -0400

    Update Pair.java

--- src/main/java/me/rina/racc/util/misc/Pair.java
+++ src/main/java/me/rina/racc/util/misc/Pair.java
@@ -1,6 +1,6 @@
 package me.rina.racc.util.misc;
 
-public class Pair <T, S>
+public class Pair<T, S>
 {
     T key;
     S value;
@@ -16,6 +16,10 @@ public class Pair <T, S>
 
     public S getValue() {
         return value;
+    }
+
+    public void setKey(T key) {
+        this.key = key;
     }
 
     public void setValue(S value) {
